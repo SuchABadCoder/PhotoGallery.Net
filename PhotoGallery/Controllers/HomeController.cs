479a47c217f096dfc00bf21142bb6f510316e4f3
SuchABadCoder <gayday57@gmail.com>
2021-07-18 17:25:56 +0300

    Added admin panel

--- PhotoGallery/Controllers/HomeController.cs
+++ PhotoGallery/Controllers/HomeController.cs
@@ -7,23 +7,45 @@ namespace PhotoGallery.Controllers
 {
     public class HomeController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private static ApplicationDbContext db = new ApplicationDbContext();
+        private static string category = "All";
+
         public ActionResult Index()
         {
             var categories = db.Categories.ToList();
-            ViewBag.photos = db.Photos.ToList();
+            ViewBag.photos = db.Photos.ToList(); ;
             return View(categories);
         }
 
         [HttpGet]
         public ActionResult Filter(string categoryName)
         {
+            var owner = User.Identity.Name;
+
             if (categoryName == "All")
-                ViewBag.photos = db.Photos.ToList();
+            {
+                ViewBag.photos = db.Photos.ToList(); 
+                category = categoryName;
+            }
             else if (categoryName == "Rate")
-                ViewBag.photos = Rate(db.Photos.ToList());
+            {
+                if (category == "All")
+                    ViewBag.photos = Rate(db.Photos.ToList());
+                else if (category == "My")
+                    ViewBag.photos = Rate(db.Photos.Where(p => p.OwnerName == owner).ToList());
+                else
+                    ViewBag.photos = Rate(db.Photos.Where(p => p.Category.Name == category).ToList());
+            }
+            else if (categoryName == "My")
+            {
+                ViewBag.photos = db.Photos.Where(p => p.OwnerName == owner).ToList();
+                category = categoryName;
+            }
             else
+            {
                 ViewBag.photos = db.Photos.Where(p => p.Category.Name == categoryName).ToList();
+                category = categoryName;
+            }
 
             var categories = db.Categories.ToList();
 
@@ -49,6 +71,22 @@ namespace PhotoGallery.Controllers
             photos.Sort(new PhotosComperer());
 
             return photos;
+        }
+
+        public ActionResult Admin()
+        {
+            var photos = db.Photos.ToList();
+            return View(photos);
+        }
+
+        public JsonResult Delete(int id)
+        {
+            var bd = new ApplicationDbContext();
+            var photo = bd.Photos.Where(p => p.Id == id).FirstOrDefault();
+            bd.Photos.Remove(photo);
+            bd.SaveChanges();
+             
+            return Json(true, JsonRequestBehavior.AllowGet);
         }
     }
 }
